a6b4ccc5e80e24e32e2112b674623ecea79ac912
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-01-28 09:21:03 +0100

    Update es33.py

--- es33.py
+++ es33.py
@@ -1,11 +1,15 @@
 '''
 In un laboratorio di analisi 
+i pazienti sono sottoposti ad un esame specialistico secondo l'ordine di arrivo.
+Scrivi un programma che consenta di registrare i nomi dei pazienti mano a mano che arrivano.
+Visualizza poi il nome del paziente che deve essere sottoposto all'esame
+perchè è il primo della coda in attesa.
 
 '''
 
 Coda =[]
-lung = int(input("Quanti pazienti sono in coda"))
-for n in range(1, lung +1):
+lunghezza = int(input("Quanti pazienti sono in coda"))
+for n in range(1, lunghezza +1):
     paziente = input("inserire il nome del paziente")
     Coda.append(paziente)
 primo_paziente = Coda[0]
